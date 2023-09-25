cc7ab515847b52678ad0671bcf32bbf2d27ed4c2
unknown <bekturtoktobekov7@gmail.com>
2023-09-25 16:00:43 +0600

    added footer

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -7,7 +7,7 @@ const Footer = ({ className, ...props} : FooterProps) => {
   return (
     <footer {...props} className={cn(className, styles.footer)}>
       <div>OwlTop co. 2022 - {format(new Date(), 'yyyy')} Все права защищены</div>
-      <a href="#" target="_blank">Пользовательское соглашение</a>
+      <a href="#" target="_blank">Пользовательское соглашение sdlfksdlfksdjlf</a>
       <a href="#" target="_blank">Политика конфиденциальности</a>
     </footer>
   )
